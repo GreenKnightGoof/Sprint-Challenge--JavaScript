8f4bfc6cec2e51ec23681f634e842c1da3bee6ba
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2020-03-24 19:00:28 -0500

    Still working on Arrays

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,6 +1,6 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
-class CuboidMaker {
+class cuboidMaker {
   constructor(length, width, height) {
     this.length = length;
     this.width = width;
@@ -15,9 +15,9 @@ class CuboidMaker {
 
 }
 
-let cuboid = new CuboidMaker = {length: 4, width: 5, height: 5};
+let Cuboid = new CuboidMaker(4, 5, 5);
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(Cuboid.volume()); // 100
+console.log(Cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
